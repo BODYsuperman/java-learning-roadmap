a498a7d657f9d4167dea12aec6b3ce0f30938fe9
alex <962213127@qq.com>
2026-01-29 07:44:55 +0800

    demo3

--- 02basic-code/src/com/alex/operator/OperatorDemo3.java
+++ 02basic-code/src/com/alex/operator/OperatorDemo3.java
@@ -26,5 +26,20 @@ public class OperatorDemo3 {
         int res2 = j++;
         System.out.println(j); //11
         System.out.println(res2); //10
+
+
+        int m = 3;
+        int n = 7;
+
+        /*
+                m 3 4 5 4 5
+                n 7 8 7
+                res3 = 3 + 5 - 4 + 8 - 8 + 4 +1;
+         */
+        int res3 = m++ + ++m - --m + ++n - n-- + m++ +1;
+        System.out.println(m); //5
+        System.out.println(n); //7
+        System.out.println(res3);//9
+
     }
 }
